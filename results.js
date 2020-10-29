179946e4e2c843873e0330a4ffe6d0edb3eeeecd
juan pena <juanpena7722@gmail.com>
2020-10-29 19:41:32 -0400

    working on get method form index.html

--- results.js
+++ results.js
@@ -1,31 +1,14 @@
-let q = "videos of earth";
+let q = "galaxies";
 $(document).ready(function() {
     $.ajax({
         METHOD: "GET",
         url: "https://images-api.nasa.gov/search?q=" + q
     }).then(function(collection) {
-        console.log(collection)
+        console.log(collection.collection)
+        for (let i in collection.collection.items) {
 
-        let newCall = collection.collection.items[0].href;
-        console.log(newCall)
-        $.ajax({
-            METHOD: "GET",
-            url: newCall
-        }).then(function(post) {
+            $('.container').append('<img class="container-img" width="300" src="' + collection.collection.items[i].links[0].href + '">');
 
-            for (let i in post) {
-                const images = ["jpg", "gif", "png"]
-                const videos = ["mp4", "3gp", "ogg"]
-
-                const url = new URL(post[i])
-                const extension = url.pathname.split(".")[1]
-
-                if (images.includes(extension)) {
-                    $('.container').append('<img src="' + post[i] + '">');
-                } else if (videos.includes(extension)) {
-                    $('.container').append('<video  ' + 'controls="' + 'control"' + 'src="' + post[i] + '">');
-                }
-            }
-        });
-    });
-});
+        }
+    })
+})
