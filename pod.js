b93d7deae73508c3299477334b625b0874167003
Alex Fernandes <alexfernands@outlook.com>
2020-10-31 09:02:26 -0400

    main page detais fix

--- pod.js
+++ pod.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-    var $background = $("#show_bg_2");
+$(document).ready(function () {
+	var $background = $("#show_bg_2");
 
-    $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=gYo6aP2v2kURjJFrRdzO4eOvx79sGG3ngPwy2USO",
-    }).then(function(data) {
-        // console.log(data.hdurl);
+	$.ajax({
+		url:
+			"https://api.nasa.gov/planetary/apod?api_key=gYo6aP2v2kURjJFrRdzO4eOvx79sGG3ngPwy2USO",
+	}).then(function (data) {
+		// console.log(data.hdurl);
 
-        $background.css(
-            "background-image",
-            "radial-gradient(circle, rgba(0,0,0,0.87718837535014) 0%, rgba(0,0,0,0.8547794117647058) 100%), url(" +
-            data.hdurl +
-            ")"
-        );
-        // $('#img').append('<img src="' +  + '">');
-        // console.log(data.hdurl);
-    });
+		$background.css(
+			"background-image",
+			"radial-gradient(circle, rgba(0,0,0,0.87718837535014) 0%, rgba(0,0,0,0.8547794117647058) 100%),   url(" +
+				data.hdurl +
+				")"
+		);
+		// $('#img').append('<img src="' +  + '">');
+		// console.log(data.hdurl);
+	});
 });
